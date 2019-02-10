6e5dc443fe0176b1b4b8f671d08cdb005b5abe5d
Cookiiiies <cookiiiies@users.noreply.github.com>
2019-02-11 00:50:03 +0100

    separate kubectl and gcloud install

--- scripts/install_gcloud.sh
+++ scripts/install_gcloud.sh
@@ -3,9 +3,9 @@
 if [ ! -d ${HOME}/google-cloud-sdk ]; then
      wget https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.zip && unzip google-cloud-sdk.zip && rm google-cloud-sdk.zip
      google-cloud-sdk/install.sh --usage-reporting=true --path-update=true
+fi
+
+if [ ! -d ${HOME}/kubectl ]; then
      $( mkdir -p ${HOME}/kubectl && cd ${HOME}/kubectl && curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl )
      chmod +x ${HOME}/kubectl/kubectl
-     kubectl version
-     gcloud --version
-     which gcloud kubectl
 fi
