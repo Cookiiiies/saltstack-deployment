3f73cdba2601770b0e3b4047c51df43c7644dbf9
Cookiiiies <cookiiiies@users.noreply.github.com>
2019-02-10 23:35:15 +0100

    changed to jinja build-in FileSystemLoader

--- scripts/render_k8s.py
+++ scripts/render_k8s.py
@@ -1,19 +1,28 @@
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 from os import environ, listdir
 
 
+# Capture our current directory
+template_dir = '{dir}/k8s'.format(
+    dir=environ['TRAVIS_BUILD_DIR']
+)
 
-for jinja_template in [filename for filename in listdir('{}/k8s'.format(environ['TRAVIS_BUILD_DIR'])) if filename.endswith(".jinja2")]:
+jinja_env = Environment(
+    loader=FileSystemLoader(template_dir),
+    trim_blocks=True
+)
+
+for jinja_template in [filename for filename in listdir(template_dir) if filename.endswith(".jinja2")]:
+    print("render file: {}".format(jinja_template))
+    rendered_file = jinja_env.get_template(jinja_template).render(
+        environ
+    )
     
-    path = "{dir}/k8s/{name}".format(dir=environ['TRAVIS_BUILD_DIR'], name=jinja_template)
-    print("openfile: {}\n".format(path))
-    with open(path) as file:
-        
-        print("render file")
-        template = Template(file.readlines())
-        output_filename = jinja_template.replace(".jinja2")
-        
-        path = "{dir}/k8s_descriptions/{name}".format(dir=environ['HOME'], name=output_filename)
-        print("output to: {}\n".format(path))
-        with open(path, "w") as output_file:
-            output_file.write(template.render(environ))
+    path = "{dir}/k8s_descriptions/{name}".format(
+        dir=environ['HOME'],
+        name=jinja_template.replace(".jinja2")
+    )
+    
+    print("output to: {}\n".format(path))
+    with open(path, "w") as output_file:
+        output_file.write(rendered_file)
