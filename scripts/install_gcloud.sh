4899b27c45c490e1130d37ce9bac872281059ade
Cookiiiies <cookiiiies@users.noreply.github.com>
2019-02-10 22:31:59 +0100

    change glcoud install to separate script

---
+++ scripts/install_gcloud.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if [ ! -d ${HOME}/google-cloud-sdk ]; then
+     wget https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.zip && unzip google-cloud-sdk.zip && rm google-cloud-sdk.zip
+     google-cloud-sdk/install.sh --usage-reporting=true --path-update=true --additional-components kubectl
+fi
