562309692daa7c50d6432c3b39ce186ecb96a28b
Cookiiiies <cookiiiies@users.noreply.github.com>
2019-02-10 23:49:19 +0100

    add base64_decode filter to jinja

--- scripts/render_k8s.py
+++ scripts/render_k8s.py
@@ -1,7 +1,13 @@
 from jinja2 import Environment, FileSystemLoader
 from os import environ, listdir
+from base64 import b64decode
+
+# custom jinja filter
+def base64_decode(value):
+    return b64decode(value)
 
 
+# init jinja
 template_dir = '{dir}/k8s'.format(
     dir=environ['TRAVIS_BUILD_DIR']
 )
@@ -10,7 +16,9 @@ jinja_env = Environment(
     loader=FileSystemLoader(template_dir),
     trim_blocks=True
 )
+jinja_env.filters['base64_decode'] = base64_decode
 
+# start rendering
 for jinja_template in [filename for filename in listdir(template_dir) if filename.endswith(".jinja2")]:
     print("render file: {}".format(jinja_template))
     rendered_file = jinja_env.get_template(jinja_template).render(
@@ -25,3 +33,5 @@ for jinja_template in [filename for filename in listdir(template_dir) if filenam
     print("output to: {}\n".format(path))
     with open(path, "w") as output_file:
         output_file.write(rendered_file)
+
+
