24b61149804f3b1912259216058035511ef69692
Cookiiiies <cookiiiies@users.noreply.github.com>
2019-02-11 00:26:40 +0100

    add version check to glcoud install

--- scripts/install_gcloud.sh
+++ scripts/install_gcloud.sh
@@ -4,4 +4,7 @@ if [ ! -d ${HOME}/google-cloud-sdk ]; then
      wget https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.zip && unzip google-cloud-sdk.zip && rm google-cloud-sdk.zip
      google-cloud-sdk/install.sh --usage-reporting=true --path-update=true --additional-components kubectl
      gcloud components install kubectl
+     kubectl version
+     gcloud --version
+     which gcloud kubectl
 fi
