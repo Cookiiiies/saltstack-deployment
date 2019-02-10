d541d36d32dc98a1d437faa0b806d8d36cffb549
Cookiiiies <cookiiiies@users.noreply.github.com>
2019-02-10 22:36:57 +0100

    change python script to absolute path

--- scripts/deploy_k8s.py
+++ scripts/deploy_k8s.py
@@ -1,6 +1,6 @@
-from os import listdir
+from os import listdir, environ
 import subprocess
 
-for deployment_yaml in [filename for filename in listdir('../k8s') if filename.endswith(".yaml")]:
+for deployment_yaml in [filename for filename in listdir('{}/k8s'.get(environ['HOME'])) if filename.endswith(".yaml")]:
     subprocess.run(["kubectl", "apply", "-f", "{}".format(deployment_yaml)])
 
