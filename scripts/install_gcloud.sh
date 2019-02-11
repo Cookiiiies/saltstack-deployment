6599f73f19d726e6c9e850241fdd139120f9df06
Cookiiiies <cookiiiies@users.noreply.github.com>
2019-02-11 01:18:48 +0100

    change kubectl and glcoud check for install

--- scripts/install_gcloud.sh
+++ scripts/install_gcloud.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
 
-if [ ! -d ${HOME}/google-cloud-sdk ]; then
-     wget https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.zip && unzip google-cloud-sdk.zip && rm google-cloud-sdk.zip
-     google-cloud-sdk/install.sh --usage-reporting=true --path-update=true
+if [ ! -x "$(command -v gcloud)" ]; then
+     $(
+        cd ${HOME} && \
+        wget https://dl.google.com/dl/cloudsdk/channels/rapid/google-cloud-sdk.zip && \
+        unzip google-cloud-sdk.zip && \
+        rm google-cloud-sdk.zip && \
+        google-cloud-sdk/install.sh --path-update=true
+     )
+
+fi
+
+
+if [ ! -x "$(command -v kubectl)" ]; then
+     $(
+        mkdir -p ${HOME}/kubectl && \
+        cd ${HOME}/kubectl && \
+        curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl && \
+        chmod +x ${HOME}/kubectl/kubectl &&
+        source ${HOME}/.bashrc
+     )
 fi
